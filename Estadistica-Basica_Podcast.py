a6c21ed544e3512cde0fdd0c918691aec1e0fdf7
BENITO CASADO <enriquebenito1987@gmail.com>
2020-07-22 11:20:36 +0200

    first commig

---
+++ Estadistica-Basica_Podcast.py
@@ -0,0 +1,24 @@
+-- Que significa R en estadistica?
+	La R es la correlacion
+	
+-- Least Squares
+	Least Squares es el metodo de encontrar la linea que mejor se ajusta a los puntos.
+
+--	Hypothesis testing -- El proceso de comprobar como de diferente data sets
+		null = son iguales
+		P-Value es lo que nos ayuda a hacer este proceso
+
+-- P values 
+	comprobar como de diferentes son dos cosas
+	pastilla A pastilla B
+	los valores de P value son 0-1 cuando mas cerca de 0 tenemos mas certeza de que son diferentes.
+	Cuanto tiene que valer p para que digamos que no hay ninguna diferencia entre los dos medicamentos que estamos comparando.
+		5% = 0.05 , es decir si nos sale un valor del 5% podemos decir que ambos medicamentos son iguales (esto es porque a la hora de hacer un experimento se estima que el 5% es erroneo)
+		ese 5% es lo que se llama los falsos positivos.
+-- En una distribucion normal, segun como de inclinada sea la curba diremos que la Standar deviation es mayor o menor.
+
+		una curva muy empinada su estandar deviation sera muy pequena (0.6)
+		una curba mas aplanada tipo meseta tendra una standar deviation grande (4)
+	
+
+		
